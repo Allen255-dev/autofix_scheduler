070519e199bba3f3c8ee2c7dcfe91c69cd841b63
Amegwu Christian Ojodu <Amegwu255@gmail.com>
2026-08-19 09:19:44 +0100

    Update pip install command to include break option

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-pip install -r requirements.txt
+pip install --break-system-packages -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput || true
 python manage.py seed_data || true
