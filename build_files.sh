84033d246f7522a3f4170b555035d25a40769b2f
Allen255-dev <amegwu255@gmail.com>
2026-08-19 09:54:30 +0100

    Fix Vercel Python deployment

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
-pip install --break-system-packages -r requirements.txt
+
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput || true
-python manage.py seed_data || true
